ab622af9a160057ca8fe74995bd419196cdea699
KrystianRusin <krystian.r@hotmail.com>
2024-01-10 17:52:23 -0500

    Random pokemon names are generated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import Header from "./components/Header";
+import Game from "./containers/Game";
 import "./App.css";
 
 function App() {
   return (
     <div className="content">
       <Header />
+      <Game />
     </div>
   );
 }
--- src/containers/Game.jsx
+++ src/containers/Game.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/Game.css";
 
 const Game = () => {
+  const [clicked, setClicked] = useState([]);
+  const [score, setScore] = useState(0);
+  const [pokemonCards, setPokemonCards] = useState([]);
+
+  useEffect(() => {
+    generatePokemon();
+  }, []);
+
+  const generatePokemon = () => {
+    const promises = [];
+    for (let i = 0; i < 10; i++) {
+      const randomId = Math.floor(Math.random() * 800) + 1; // Pokemon API currently has data till ID 898
+      promises.push(
+        fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`).then(
+          (response) => response.json()
+        )
+      );
+    }
+    Promise.all(promises).then((pokemonData) => {
+      setPokemonCards(pokemonData.map((pokemon) => pokemon.name));
+    });
+  };
+
   return <div>Game</div>;
 };
 
